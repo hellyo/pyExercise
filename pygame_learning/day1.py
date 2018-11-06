febbd8dc15e052b48b327b2e15455e169e220352
zac <zacmyx@gmail.com>
2018-11-06 23:10:40 +0800

    还是得看着教程大概先写写

---
+++ pygame_learning/day1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#coding=utf-8
+# pygame 
+
+import pygame
+from pygame import locals
+from sys import exit
+import time
+
+SCREEN_SIZE = (512,256)
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
+#title
+pygame.display.set_caption("test1")
+
+backImg = "back.png"
+mouseImg = "mouse.png"
+
+back = pygame.image.load(backImg).convert_alpha()
+mouse = pygame.image.load(mouseImg).convert_alpha()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == locals.QUIT:
+            exit()
+    screen.blit(back,(0,0))
+    x,y = pygame.mouse.get_pos()
+    x -= mouse.get_width()/2
+    y -= mouse.get_height()/2
+
+    screen.blit(mouse,(x,y))
+    pygame.display.update()
+    time.sleep(0.025)    
+
