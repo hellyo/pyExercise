b4b26881a03db49c85cf9c7635e82d527e587b52
zac <zacmyx@gmail.com>
2018-11-07 23:22:21 +0800

    拖动改变窗口大小

--- pygame_learning/test4.py
+++ pygame_learning/test4.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #coding=utf-8
-# pygame切换窗口大小
+# pygame拖动窗口大小
 
 import pygame
 from pygame import locals
@@ -11,23 +11,23 @@ backImg = "back.png"
 
 SCREEN_SIZE = (512,256)
 pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
+screen = pygame.display.set_mode(SCREEN_SIZE,locals.RESIZABLE,32)
 back = pygame.image.load(backImg)
 #title
 FullScreen = False
 pygame.display.set_caption("test4")
 
 while True:
-    for event in pygame.event.get():        
-        if event.type == locals.QUIT:
-            exit()
-        if event.type == locals.KEYDOWN:
-            if event.key == locals.K_f:
-                FullScreen = not FullScreen
-            if FullScreen:
-                screen = pygame.display.set_mode(SCREEN_SIZE,locals.FULLSCREEN,0)
-            else:
-                screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
-    screen.blit(back,(0,0))
+    event = pygame.event.wait()      
+    if event.type == locals.QUIT:
+        exit()
+    if event.type == locals.VIDEORESIZE:
+        SCREEN_SIZE = event.size
+        screen = pygame.display.set_mode(SCREEN_SIZE,locals.RESIZABLE,32)
+        pygame.display.set_caption("resized to " + str(event.size))
+    w,h = SCREEN_SIZE
+    for y in range(0,h,back.get_height()):
+        for x in range(0,w,back.get_width()):
+            screen.blit(back,(x,y))
     pygame.display.update()
     time.sleep(0.025)
---
+++ pygame_learning/test5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#coding=utf-8
+# pygame切换窗口大小
+
+import pygame
+from pygame import locals
+from sys import exit
+import time
+
+backImg = "back.png"
+
+SCREEN_SIZE = (512,256)
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
+back = pygame.image.load(backImg)
+#title
+FullScreen = False
+pygame.display.set_caption("test5")
+
+while True:
+    for event in pygame.event.get():        
+        if event.type == locals.QUIT:
+            exit()
+        if event.type == locals.KEYDOWN:
+            if event.key == locals.K_f:
+                FullScreen = not FullScreen
+            if FullScreen:
+                screen = pygame.display.set_mode(SCREEN_SIZE,locals.FULLSCREEN,0)
+            else:
+                screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
+    screen.blit(back,(0,0))
+    pygame.display.update()
+    time.sleep(0.025)
